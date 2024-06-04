d2179337143cb884caffa62268078aedd041c299
Konstantin Smirnov <sasharugby@yandex.ru>
2024-06-04 18:56:00 +0300

    12done

---
+++ 2rab/Program.cs
@@ -0,0 +1,13 @@
+﻿int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+List<int> list = new List<int>();
+
+foreach( int x in a)
+{
+    list.Add(x);
+}
+list.Sort();
+
+foreach(int x in list)
+{
+    Console.WriteLine(x);
+}    
